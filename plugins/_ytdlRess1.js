b20d7abb56000a7e1f473b87da91da9b55f30b8a
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-11-07 07:01:16 +0700

    a

--- plugins/_ytdlRess1.js
+++ plugins/_ytdlRess1.js
@@ -1,16 +1,16 @@
 let limit = 30
-let fs = require('fs')
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 2050 < filesize
- 
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+ let fs = require('fs')
+ let y = fs.readFileSync('./src/thumb.jpeg')
 
 
-conn.sendMessage(m.chat, `*Title:* ${title}\n*Size:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: fs.readFileSync('./src/thumb.jpeg'), contextInfo:{externalAdReply: {title: 'Simple WhatsApp bot', body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: fs.readFileSync('./src/thumb.jpeg')}}})
-  if (!isLimit) conn.sendFile(m.chat, dl_link , 'conversation', {quoted: m, thumbnail: fs.readFileSync('./src/thumb.jpeg'), contextInfo:{externalAdReply: {title: `${title}`, body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: }}}, m, false, {ptt: true, duration: 999999999999})
+conn.sendMessage(m.chat, `*Title:* ${title}\n*Size:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: global.thumb2, contextInfo:{externalAdReply: {title: 'Simple WhatsApp bot', body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: y}}})
+  if (!isLimit) conn.sendFile(m.chat, dl_link , 'conversation', {quoted: m, thumbnail: y, contextInfo:{externalAdReply: {title: `${title}`, body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: y}}}, m, false, {ptt: true, duration: 999999999999})
 }
 handler.command = /^dlmsc$/i
 handler.owner = false
@@ -27,3 +27,4 @@ handler.exp = 0
 handler.limit = true
 
 module.exports = handler
+
