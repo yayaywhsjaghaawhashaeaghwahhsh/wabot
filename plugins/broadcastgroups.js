aedd26dbe5f0c117055384976937c48c914855e5
Rlxfly <87053694+Rlxfly@users.noreply.github.com>
2021-11-10 10:39:15 +0700

    Y

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,10 +1,39 @@
 let handler  = async (m, { conn, text }) => {
+  let fs = require('fs')
+  let fetch = require('node-fetch')
+  const {
+    MessageType,
+    Mimetype
+} = require("@adiwajshing/baileys");
+const anu = {
+	"key": {
+		"fromMe": false,
+		"participant": "0@s.whatsapp.net",
+		"remoteJid": "0@s.whatsapp.net"
+	},
+	"message": {
+		"groupInviteMessage": {
+			"groupJid": "6285240750713-1610340626@g.us",
+			"inviteCode": "mememteeeekkeke",
+			"groupName": "P", 
+            "caption": "「 All Group Broadcast 」", 
+            'jpegThumbnail': fs.readFileSync('./src/thumb.jpeg')
+		}
+	}
+}
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? itsu.user.jid : m.sender
+  try {
+    pp = await conn.getProfilePicture(who)}
+    catch (e){
+    }
+
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
+  let content = await conn.cMod(m.chat, cc, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Group Broadcast 」')
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Group Broadcast 」\n' + randomID(32)), true).catch(_=>_)
-  m.reply('Selesai Broadcast All Group :)')
+  for (let id of groups) conn.copyNForward(id, content, 'conversation', {quoted: anu, thumbnail: fs.readFileSync('./src/thumb.jpeg'), contextInfo:{externalAdReply: {title: `© ${conn.user.name} || by Relldev` , body: '>///<',sourceUrl: 'https://chat.whatsapp.com/J3j8XFLPnOR0RI937C8Biu', thumbnail: fs.readFileSync('./src/thumb.jpeg')}}} ,true)
+  conn.reply(m.chat, `_Done_`, m)
 }
 handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
@@ -26,3 +55,5 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+
