bd584c5b3ba7c26613866329f37e70c077b45f5b
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-11-18 17:47:51 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn, args }) => {
   let res = await fetch(API('Velgrynd', '/api/tiktok2', { url: args[0] }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let url = await shortUrl(json.result.nowm)
-  conn.sendFile(m.chat, url, 'tiktok.mp4', '*Link:* ' + url, m)
+  let { nowm } = json.result
+  conn.sendFile(m.chat, nowm, 'tiktok.mp4', '*Link:* ' + url, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
@@ -15,7 +15,3 @@ handler.command = /^(tik(tok)?(dl)?)$/i
 handler.limit = true
 
 module.exports = handler
-
-async function shortUrl(url) {
-  return await (await fetch('https://tinyurl.com/api-create.php?url=' + encodeURIComponent(url))).text()
-}
