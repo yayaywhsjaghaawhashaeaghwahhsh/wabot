d38777aced2a15975be40f85858394d1deb1859e
LitRHap <73509282+LitRHap@users.noreply.github.com>
2021-12-10 07:51:00 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -4,7 +4,11 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link Salah'
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+        var jumlahHari = 86400000 * 0.5
+        var now = new Date() * 1
+        if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
+        else global.db.data.chats[res.gid].expired = now + jumlahHari
     })
     await conn.sendButton(res.gid, `
 *${conn.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, *${conn.user.name}* diundang oleh @${m.sender.split`@`[0]}
