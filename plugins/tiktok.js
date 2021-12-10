a57cc552dcb81db377359e5cffc82b034dd2173e
Ichika (≧ ∇ ≦) <81684610+ultimareall@users.noreply.github.com>
2021-12-10 08:27:22 +0700

    Alip Ganteng

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
   let res = await fetch(API('Velgrynd', '/api/tiktok2', { url: args[0] }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let url = json.video_original
+  let url = json.result.video_url
   conn.sendFile(m.chat, url, 'tiktok.mp4', '', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
