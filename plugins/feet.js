acd58d4aed35965a71b4f2834d43c8a87ffbed57
LitRHap <73509282+LitRHap@users.noreply.github.com>
2021-12-18 12:41:52 +0700

    Update feet.js

--- plugins/feet.js
+++ plugins/feet.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage/feet?apikey=79lJ9HEA`
+  let res = `https://api.ichikaa.xyz/api/randomimage?q=feet&apikey=AAgXXQeo`
   conn.sendFile(m.chat, res, 'feet.jpg', 'huu suka ama kaki', m)
 }
 handler.help = ['feet']
