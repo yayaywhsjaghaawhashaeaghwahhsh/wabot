5f60caa135a8c95fedd0703829b2d24254bac202
L I N G Z <81684610+ultimareall@users.noreply.github.com>
2021-12-18 21:24:19 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,13 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
-  let res = await fetch(global.API('zeks', '/api/pinimg', {
-    q: encodeURI(text)
-  }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  if (!text) throw `Contoh: ${usedPrefix + command} ichika amasawa`
+  let res = await fetch(global.API('Velgrynd', '/api/pinterest', { query: encodeURI(text) }, 'apikey'))
+  if (!res.ok) throw await `${res.status}`
   let json = await res.json()
   if (!json.status) throw json
-  let pint = json.data[Math.floor(Math.random() * json.data.length)];
+  let pint = json.result[Math.floor(Math.random() * json.result.length)];
   conn.sendFile(m.chat, pint, '', `
 *Hasil pencarian*
 ${text}
