669a89ac3b79f12dea41f5f2cc53df9400b07b68
S H I R O <81684610+ultimareall@users.noreply.github.com>
2021-12-24 15:46:54 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(API('Velgrynd', '/api/tiktok', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('Velgrynd', '/api/snaptik', { url: args[0] }, 'apikey'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  conn.sendFile(m.chat, json.result.url_nowm, 'tiktok.mp4', json.result.desc, m)
+  conn.sendFile(m.chat, json.result.video.no_watermark, 'tiktok.mp4', json.result.description, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
