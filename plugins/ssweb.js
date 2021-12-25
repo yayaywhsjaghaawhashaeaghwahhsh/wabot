61d87bdb5d3c1ea8638eb8a0f6f2b7252a3547f9
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-12-25 12:40:47 +0700

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -1,9 +1,8 @@
 let handler = async (m, { conn, command, args }) => {
-  let full = /f$/i.test(command) ? 'on' : 'off'
-  if (!args[0]) return m.reply('Tidak ada url')
+  if (!args[0]) throw 'Tidak ada url'
   await m.reply('Loading...')
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-  let ss = 'http://hadi-api.herokuapp.com/api/ssweb?url=' + url + '&device=desktop&full=' + full
+  let ss = /f$/i.test(command) ? API('http://hadi-api.herokuapp.com', '/api/ssweb2', { url }) : API('https://api.popcat.xyz', '/screenshot', { url })
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m)
 }
 
