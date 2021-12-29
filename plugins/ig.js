8fa65f276777668888c6060b953ac89a088c97a0
Ripp <75048902+Akkun3704@users.noreply.github.com>
2021-12-30 03:08:25 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,15 +1,10 @@
-let fetch = require('node-fetch')
+let scraper = require('@bochilteam/scraper')
 
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(API('https://megayaa.herokuapp.com', '/api/igdl', { url: args[0] }))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (json.status != true) throw json
-  for (let i = 0; i < json.result.length; i++) {
-    let capt = i == 0 ? json.caption : ''
-    conn.sendFile(m.chat, json.result[i].url, '', capt, m)
-  }
+  await m.reply('Loading...')
+  let res = await scraper.instagramdl(args[0])
+  for (let i = 0; i < res.length; i++) await conn.sendFile(m.chat, res[i].url, '', '', m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
