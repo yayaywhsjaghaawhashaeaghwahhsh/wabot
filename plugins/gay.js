2f9e50494f60a7fe7376122bf646376c462acf5b
S H I R O <81684610+ultimareall@users.noreply.github.com>
2021-12-29 18:17:37 +0700

    Salah tadi:v

--- plugins/gay.js
+++ plugins/gay.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
    let numb = Math.floor(Math.random() * 100) + 1
    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-   conn.sendFile(m.chat, API('Velgrynd', '/seberapagay', { name: conn.getName(who), image: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'), num: numb }, 'apikey'), 'gay.png', 'Lariii ada gay', m)
+   conn.sendFile(m.chat, API('Velgrynd', '/api/seberapagay', { name: conn.getName(who), image: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'), num: numb }, 'apikey'), 'gay.png', 'Lariii ada gay', m)
 }
 handler.help = ['gay']
 handler.tags = ['maker']
